d19129c81bcd7b53c9db6b81719bd42da6815bee
Yosuke Torii <jinjorweb@gmail.com>
2013-04-08 23:17:01 +0900

    なんでもないコミット

--- sample/out/js/sample.js
+++ sample/out/js/sample.js
@@ -244,6 +244,7 @@ org.jinjor.haxeminesample.Main.__name__ = true;
 org.jinjor.haxeminesample.Main.main = function() {
 	console.log("Hello.");
 	org.jinjor.haxeminesample.Main.sub();
+	new org.jinjor.haxeminesample.util.Util3();
 }
 org.jinjor.haxeminesample.Main.sub = function() {
 }
@@ -257,6 +258,12 @@ org.jinjor.haxeminesample.util.Util2 = function() { }
 org.jinjor.haxeminesample.util.Util2.__name__ = true;
 org.jinjor.haxeminesample.util.Util2.join = function(a,b) {
 	if(a.charAt(a.length - 1) == "/" && b.charAt(0) == "/") return a + b.substring(1); else if(a.charAt(a.length - 1) != "/" && b.charAt(0) != "/") return a + "/" + b; else return a + b;
+}
+org.jinjor.haxeminesample.util.Util3 = function() {
+};
+org.jinjor.haxeminesample.util.Util3.__name__ = true;
+org.jinjor.haxeminesample.util.Util3.prototype = {
+	__class__: org.jinjor.haxeminesample.util.Util3
 }
 if(Array.prototype.indexOf) HxOverrides.remove = function(a,o) {
 	var i = a.indexOf(o);
