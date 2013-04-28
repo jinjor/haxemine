5864b670565d71757481a2d71b57138ab2c981d2
Yosuke Torii <jinjorweb@gmail.com>
2013-04-28 12:06:16 +0900

    ロジック改良

--- sample/run.js
+++ sample/run.js
@@ -21,75 +21,70 @@ page.open('local.html', function(){
   
   page.injectJs('jasmine-1.3.1/jasmine.js');
   //page.injectJs('jasmine-1.3.1/jasmine-html.js');
-  page.evaluate(function() {
-      if (! jasmine) {
-          throw new Exception("jasmine library does not exist in global namespace!");
-      }
-       
-      var ConsoleReporter = function() {
-          this.started = false;
-          this.finished = false;
-      };
-      ConsoleReporter.prototype = {
-          reportRunnerResults: function(runner) {
-              var dur = (new Date()).getTime() - this.start_time;
-              var failed = this.executed_specs - this.passed_specs;
-              var spec_str = this.executed_specs + (this.executed_specs === 1 ? " spec, " : " specs, ");
-              var fail_str = failed + (failed === 1 ? " failure in " : " failures in ");
-
-              this.log("Runner Finished.");
-              this.log(spec_str + fail_str + (dur/1000) + "s.");
-                  
-              this.finished = true;
-          },
-
-          reportRunnerStarting: function(runner) {
-              this.started = true;
-              this.start_time = (new Date()).getTime();
-              this.executed_specs = 0;
-              this.passed_specs = 0;
-              this.log("Runner Started.");
-          },
-
-          reportSpecResults: function(spec) {
-              var resultText = "Failed.";
-              if (spec.results().passed()) {
-                  this.passed_specs++;
-                  resultText = "Passed.";
-              }
-
-              this.log(resultText);
-          },
-
-          reportSpecStarting: function(spec) {
-              this.executed_specs++;
-              this.log(spec);
-              this.log(spec.suite);
-              this.log(spec.suite.description + ' : ' + spec.description + ' ... ');
-          },
-
-          reportSuiteResults: function(suite) {
-              var results = suite.results();
-              this.log(suite.description + ": " + results.passedCount + " of " + results.totalCount + " passed.");
-          },
-
-          log: function(str) {
-              //var console = jasmine.getGlobal().console;
-              console.log('jasmine > ' + str);
-          }
-      };
-      
-      // export public
-      jasmine.ConsoleReporter = ConsoleReporter;
-  });
   //console.log(page.offlineStoragePath);
   getSources().concat(getSpecs()).forEach(function(path){
     page.injectJs(path);
   });
   page.evaluate(function() {
+    if (! jasmine) {
+        throw new Exception("jasmine library does not exist in global namespace!");
+    }
+    var ConsoleReporter = function() {
+        this.started = false;
+        this.finished = false;
+    };
+    ConsoleReporter.prototype = {
+        reportRunnerResults: function(runner) {
+            var dur = (new Date()).getTime() - this.start_time;
+            var failed = this.executed_specs - this.passed_specs;
+            var spec_str = this.executed_specs + (this.executed_specs === 1 ? " spec, " : " specs, ");
+            var fail_str = failed + (failed === 1 ? " failure in " : " failures in ");
+
+            this.log("Runner Finished.");
+            this.log(spec_str + fail_str + (dur/1000) + "s.");
+                
+            this.finished = true;
+        },
+
+        reportRunnerStarting: function(runner) {
+            this.started = true;
+            this.start_time = (new Date()).getTime();
+            this.executed_specs = 0;
+            this.passed_specs = 0;
+            this.log("Runner Started.");
+        },
+
+        reportSpecResults: function(spec) {
+            var resultText = "Failed.";
+            if (spec.results().passed()) {
+                this.passed_specs++;
+                resultText = "Passed.";
+            }
+
+            this.log(resultText);
+        },
+
+        reportSpecStarting: function(spec) {
+            this.executed_specs++;
+            this.log(spec);
+            this.log(spec.suite);
+            this.log(spec.suite.description + ' : ' + spec.description + ' ... ');
+        },
+
+        reportSuiteResults: function(suite) {
+            var results = suite.results();
+            this.log(suite.description + ": " + results.passedCount + " of " + results.totalCount + " passed.");
+        },
+
+        log: function(str) {
+            //var console = jasmine.getGlobal().console;
+            console.log('jasmine > ' + str);
+        }
+    };
+    
     var jasmineEnv = jasmine.getEnv();
     jasmineEnv.updateInterval = 1000;
-    jasmineEnv.addReporter(new jasmine.ConsoleReporter());
+    jasmineEnv.addReporter(new ConsoleReporter());
     jasmineEnv.execute();
   });
   
